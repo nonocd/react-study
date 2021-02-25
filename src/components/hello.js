b1bb9c3b20fef8cc5b2f477b48da3070b2efc3f6
nonocd <nonocdhuang@gmail.com>
2021-02-25 16:20:20 +0800

    project init

---
+++ src/components/hello.js
@@ -0,0 +1,13 @@
+import { DatePicker } from "antd";
+import "antd/dist/antd.css";
+
+function Hello() {
+  return (
+    <>
+      <h1>Hello, World</h1>
+      <DatePicker />
+    </>
+  );
+}
+
+export default Hello;
